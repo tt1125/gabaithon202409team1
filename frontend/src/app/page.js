862907661c7e4d6cd29195e5e0b02631afa102ce
tt1125 <tsurutsuru275@gmail.com>
2024-08-31 04:46:36 +0900

    chore: Update frontend dependencies and add Map component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,9 @@
+import Map from "@/components/Map";
+
 export default function Home() {
   return (
     <main>
-      hello world
+      <Map defaultPosition={{ lat: 35.681236, lng: 139.767125 }} />
     </main>
   );
 }
