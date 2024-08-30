c09faf47ed13148b9865e64b7fee8e457ea1ee85
tt1125 <tsurutsuru275@gmail.com>
2024-08-31 06:47:42 +0900

    chore: Update frontend dependencies and add Map and Chat components to page layout

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,17 @@
 import Chat from "@/components/Chat";
 import Map from "@/components/Map";
+import { mediaQuery } from "@/styles/globals";
+import { useMediaQuery } from "@mui/material";
+
+
 
 export default function Home() {
+
+  const isMobileSize = useMediaQuery(mediaQuery.sp)
   return (
     <main style={{height : '100%' , width : '100%',display : 'flex'}}>
-      <div style={{width : '70%'}}><Map defaultPosition={{ lat: 35.681236, lng: 139.767125 }} /></div>
-      <div style={{width : '30%'}}><Chat /></div>
+      <div style={{width : isMobileSize ? '100%' : '70%'}}><Map defaultPosition={{ lat: 35.681236, lng: 139.767125 }} /></div>
+      <div style={{width : isMobileSize ? '100%' : '30%'}}><Chat /></div>
     </main>
   );
 }
---
+++ frontend/src/styles/globals.js
@@ -0,0 +1,6 @@
+export const mediaQuery = {
+    sp: '(width < 768px)',
+    tablet: '(768px <= width < 1024px)',
+    pc: '(1024px <= width)',
+  }
+  
